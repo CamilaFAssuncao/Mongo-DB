446b6fb306d3d0e9364c2f43517a1c1566acdc70
Camila Fernandes Assunção <129509305+CamilaFAssuncao@users.noreply.github.com>
2023-08-10 11:21:25 +0200

    inserting data before connecting to db

--- server.js
+++ server.js
@@ -12,25 +12,37 @@ DbConnect();
 
 const collection = client.db('test').collection('students');
 
-collection.insertOne({
+const studentData = {
   name: 'John Doe',
   age: 25,
   grade: 'A'
-}, (err, result) => {
+};
+
+console.log('Inserting data into the collection...');
+
+collection.insertOne(studentData, (err, result) => {
   if (err) {
-    console.error(err);
+    console.error('Error inserting data:', err);
     return;
   }
 
   console.log(`Inserted ${result.insertedCount} documents into the collection`);
+
+  // Query data
+  console.log('Querying data from the collection...');
+
+  collection.find({}).toArray((err, docs) => {
+    if (err) {
+      console.error('Error querying data:', err);
+      return;
+    }
+
+    console.log(`Found ${docs.length} documents in the collection`);
+    console.log(docs);
+
+    // Close the connection to the database
+    client.close();
+    console.log('Connection closed.');
+  });
 });
 
-collection.find({}).toArray((err, docs) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  console.log(`Found ${docs.length} documents in the collection`);
-  console.log(docs);
-});
